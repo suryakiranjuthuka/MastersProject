f935b1d0040e09cce5041086777736c1e223bf37
Surya Kiran Juthuka <suryakiran.kittu@gmail.com>
2015-12-10 15:48:20 -0500

    uncommented out Push_data.php

--- app/includes/push_data.php
+++ app/includes/push_data.php
@@ -4,82 +4,82 @@
   $jsondata = file_get_contents('../data/data.json');
   $json = json_decode($jsondata, true);
   
-// //******************* IMPORT HOME FROM DATA.JSON *********************
- // $allHome = $json['home'];
- // 
- // foreach ($allHome as $name => $home) {
- //     $params['body'][] = [
- //         'index' => [
- //             '_index' => 'home',
- //             '_type' => $name
- //            //  '_id' => $category['id'],
- //         ],
- //     ];
- // 
- //     $params['body'][] = [
- //         $name => $home
- //        //  'risk' => $home['subParas']
- //     ];
- //     
- //    //  echo $name;
- //    //  echo '<pre>',print_r($params),'</pre>';
- // 
- // }
- // 
- // $responses = $ES->bulk($params);
- // 
- // echo '<pre>',print_r($responses),'</pre>';
+//******************* IMPORT HOME FROM DATA.JSON *********************
+ $allHome = $json['home'];
+ 
+ foreach ($allHome as $name => $home) {
+     $params['body'][] = [
+         'index' => [
+             '_index' => 'home',
+             '_type' => $name
+            //  '_id' => $category['id'],
+         ],
+     ];
+ 
+     $params['body'][] = [
+         $name => $home
+        //  'risk' => $home['subParas']
+     ];
+     
+    //  echo $name;
+    //  echo '<pre>',print_r($params),'</pre>';
+ 
+ }
+ 
+ $responses = $ES->bulk($params);
+ 
+ echo '<pre>',print_r($responses),'</pre>';
  
  
- // //******************* IMPORT PUBLICATIONS FROM DATA.JSON *********************
-  // $allPublications = $json['publications'];
-  // 
-  // foreach ($allPublications as $key => $publications) {
-  //     $i = 1;
-  //     foreach ($publications as $row) {
-  //         $params['body'][] = [
-  //             'index' => [
-  //                 '_index' => 'publications',
-  //                 '_type' => $key,
-  //                 '_id' => $i,
-  //             ],
-  //         ];
-  // 
-  //         $params['body'][] = [
-  //             'point' => $row['point']
-  //         ];
-  //         ++$i;
-  //     }
-  // }
-  // 
-  // $responses = $ES->bulk($params);
-  // echo '<pre>',print_r($responses),'</pre>';
+ //******************* IMPORT PUBLICATIONS FROM DATA.JSON *********************
+  $allPublications = $json['publications'];
+  
+  foreach ($allPublications as $key => $publications) {
+      $i = 1;
+      foreach ($publications as $row) {
+          $params['body'][] = [
+              'index' => [
+                  '_index' => 'publications',
+                  '_type' => $key,
+                  '_id' => $i,
+              ],
+          ];
+  
+          $params['body'][] = [
+              'point' => $row['point']
+          ];
+          ++$i;
+      }
+  }
+  
+  $responses = $ES->bulk($params);
+  echo '<pre>',print_r($responses),'</pre>';
  
 
 
-// //******************* IMPORT GRAD STUDENTS FROM DATA.JSON *********************
-  //  $allGradStudents = $json['gradStudents'];
-  //  
-  //  foreach ($allGradStudents as $key => $gradStudents) {
-  //      $i = 1;
-  //      foreach ($gradStudents as $row) {
-  //          $params['body'][] = [
-  //              'index' => [
-  //                  '_index' => 'grad-students',
-  //                  '_type' => $key,
-  //                  '_id' => $i,
-  //              ],
-  //          ];
-  //  
-  //          $params['body'][] = [
-  //              'point' => $row['point']
-  //          ];
-  //          ++$i;
-  //      }
-  //  }
-  //  
-  //  $responses = $ES->bulk($params);
-  //  echo '<pre>',print_r($responses),'</pre>';
+//******************* IMPORT GRAD STUDENTS FROM DATA.JSON *********************
+   $allGradStudents = $json['gradStudents'];
+   
+   foreach ($allGradStudents as $key => $gradStudents) {
+       $i = 1;
+       foreach ($gradStudents as $row) {
+           $params['body'][] = [
+               'index' => [
+                   '_index' => 'grad-students',
+                   '_type' => $key,
+                   '_id' => $i,
+               ],
+           ];
+   
+           $params['body'][] = [
+               'point' => $row['point']
+           ];
+           ++$i;
+       }
+   }
+   
+   $responses = $ES->bulk($params);
+   echo '<pre>',print_r($responses),'</pre>';
 
 
 
